cc8257bcbf88611107c1a04f4413b07a82edaa2a
Fatih Rizqon <fatihrizqon@gmail.com>
2018-03-22 16:31:03 +0700

    New Refreshment

--- database/amana.sql
+++ database/amana.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 16, 2018 at 09:47 AM
+-- Generation Time: Mar 22, 2018 at 10:30 AM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.2
 
@@ -25,26 +25,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `mobil`
+-- Table structure for table `kendaraan`
 --
 
-CREATE TABLE `mobil` (
-  `id_mobil` int(5) NOT NULL,
-  `tipe` varchar(20) NOT NULL,
+CREATE TABLE `kendaraan` (
+  `id_kendaraan` int(5) NOT NULL,
+  `jenis` int(1) NOT NULL DEFAULT '0',
+  `tipe` varchar(100) NOT NULL,
   `plat_nomor` varchar(14) NOT NULL,
   `warna` varchar(15) NOT NULL,
   `transmisi` varchar(5) NOT NULL,
+  `tahun` varchar(4) NOT NULL,
   `harga` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `mobil`
+-- Dumping data for table `kendaraan`
 --
 
-INSERT INTO `mobil` (`id_mobil`, `tipe`, `plat_nomor`, `warna`, `transmisi`, `harga`) VALUES
-(1, 'K410IB', 'AB 1234 SR', 'Hitam', 'A/T', 500000),
-(2, 'OC500RF 2542', 'AB 4321 SR', 'Putih', 'A/T', 500000),
-(3, 'Avanza', 'AB 7117 XZ', 'Hitam', 'A/T', 200000);
+INSERT INTO `kendaraan` (`id_kendaraan`, `jenis`, `tipe`, `plat_nomor`, `warna`, `transmisi`, `tahun`, `harga`) VALUES
+(1, 1, 'Grand New Avanza', 'AB 1836 CF', 'Hitam', 'M/T', '2017', 350000),
+(2, 1, 'Grand New Avanza', 'AB 1848 MH', 'Putih', 'M/T', '2016', 350000),
+(3, 1, 'Grand New Avanza', 'AB XXXX XY', 'Hitam', 'A/T', '2018', 350000),
+(4, 1, 'Grand New Innova Reborn', 'AB 1337 CF', 'Hitam', 'M/T', '2017', 500000),
+(5, 1, 'All New Avanza', 'AB 1145 IH', 'Silver', 'M/T', '2015', 325000),
+(6, 1, 'All New Avanza', 'AB 1056 PN', 'Grey', 'M/T', '2014', 325000),
+(7, 1, 'Brio', 'AB 1391 BF', 'Hitam', 'M/T', '2017', 0);
 
 -- --------------------------------------------------------
 
@@ -56,7 +62,7 @@ CREATE TABLE `rental` (
   `id_rental` int(10) NOT NULL,
   `nama` varchar(100) NOT NULL,
   `telepon` varchar(14) NOT NULL,
-  `id_mobil` int(5) NOT NULL,
+  `id_kendaraan` int(5) NOT NULL,
   `tanggal_pesan` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `status` int(1) NOT NULL DEFAULT '0',
   `pembayaran` int(100) NOT NULL DEFAULT '0',
@@ -64,13 +70,6 @@ CREATE TABLE `rental` (
   `tanggal_kembali` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `id_users` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `rental`
---
-
-INSERT INTO `rental` (`id_rental`, `nama`, `telepon`, `id_mobil`, `tanggal_pesan`, `status`, `pembayaran`, `tanggal_sewa`, `tanggal_kembali`, `id_users`) VALUES
-(22, 'Jaressh', '14022', 1, '2018-03-15 17:00:00', 1, 100000, '2018-03-15 17:00:00', '2018-03-16 17:00:00', 1);
 
 -- --------------------------------------------------------
 
@@ -97,17 +96,17 @@ INSERT INTO `users` (`id_users`, `username`, `password`, `created_at`) VALUES
 --
 
 --
--- Indexes for table `mobil`
+-- Indexes for table `kendaraan`
 --
-ALTER TABLE `mobil`
-  ADD PRIMARY KEY (`id_mobil`);
+ALTER TABLE `kendaraan`
+  ADD PRIMARY KEY (`id_kendaraan`);
 
 --
 -- Indexes for table `rental`
 --
 ALTER TABLE `rental`
   ADD PRIMARY KEY (`id_rental`),
-  ADD KEY `id_mobil` (`id_mobil`),
+  ADD KEY `id_mobil` (`id_kendaraan`),
   ADD KEY `id_users` (`id_users`);
 
 --
@@ -122,16 +121,16 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `mobil`
+-- AUTO_INCREMENT for table `kendaraan`
 --
-ALTER TABLE `mobil`
-  MODIFY `id_mobil` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `kendaraan`
+  MODIFY `id_kendaraan` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `rental`
 --
 ALTER TABLE `rental`
-  MODIFY `id_rental` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id_rental` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -148,7 +147,7 @@ ALTER TABLE `users`
 --
 ALTER TABLE `rental`
   ADD CONSTRAINT `rental_ibfk_1` FOREIGN KEY (`id_users`) REFERENCES `users` (`id_users`),
-  ADD CONSTRAINT `rental_ibfk_2` FOREIGN KEY (`id_mobil`) REFERENCES `mobil` (`id_mobil`);
+  ADD CONSTRAINT `rental_ibfk_2` FOREIGN KEY (`id_kendaraan`) REFERENCES `kendaraan` (`id_kendaraan`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
